cff7620ae92095370e06f0e8011d55781b4a7ad1
Abhishek Sahu <abhishekkumarsahu532@gmail.com>
2024-10-20 19:10:26 +0530

    Added pizza images

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ArrowRight from '../Components/Icons/ArrowRight'
+import PizzaImage from "../assets/Images/pizza1.png"
 
 const Home = () => {
   return (
     <div>
+        {/** Hero Section */}
         <section className=' flex flex-col-reverse items-center justify-center py-5 md:flex-row md:gap-7 bg-gradient-to-r from-amber-50 to-orange-400'>
+        
+        {/** Services Section */}
 
             <div className='w-4/6 ml-4 text-center md:w-2/6 md:text-left'>
 
@@ -33,6 +37,21 @@ const Home = () => {
                 
 
             </div>
+
+            <div>
+                <img
+                    src={PizzaImage}
+                    alt="Pizza Image"
+                    height = {600}
+                    width={600}
+
+                />
+            </div>
+
+        </section>
+
+        {/** Services Section */}
+        <section className>
 
         </section>
     </div>
